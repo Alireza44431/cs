582891fb992b8fae52d7f3d68a81b34a472f51b9
Alireza44431 <117797481+Alireza44431@users.noreply.github.com>
2022-11-29 11:35:20 +0330

    t_1

---
+++ 1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x=Convert.ToInt32(Console.ReadLine());
+        int y=Convert.ToInt32(Console.ReadLine());
+       Double z= Math.Abs((Math.Pow(x, y) - Math.Sqrt(Math.Abs(x))) / (Math.Abs(y) + x));
+        Console.WriteLine(z);
+    }
+}
---
+++ 2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[]args)
+        {
+            bool content = true;
+            bool nocontant =false;
+            Console.WriteLine("text is {0}", content);
+            Console.WriteLine("text is {0}",nocontant);
+        }
+    }
+}

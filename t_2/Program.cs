64785c2cb5f083d9a6dbfa9e023ae31636e6f602
Alireza44431 <117797481+Alireza44431@users.noreply.github.com>
2022-12-30 12:10:11 +0330

    takalif

---
+++ t_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("inter number : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("inter number : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("inter number : ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            if (a + c > b && a + b > c && b + c > a)
+                Console.WriteLine($"the triangle is formed with the primeter of => '{Perimeter(a, b, c)}'");
+            else
+                Console.WriteLine("triangle is not formed");
+            Console.ReadKey();
+        }
+        private static double Perimeter(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        }
+}
---
+++ t_3/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("inter number : ");
+            double x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("inter number : ");
+            double y = Convert.ToInt32(Console.ReadLine());
+            double abs = Math.Abs(x - y);
+            double pow = Math.Pow(abs, Math.Abs(y));
+            double sqrt = Math.Sqrt(pow);
+            Console.WriteLine("the abs of ( x - y ) is => {0} " +
+                "\nthe pow of ( ( x - y ) ^ y ) is  => {1}" +
+                " \nthe final answer is => {2}", abs, pow, sqrt);
+        }
+    }
+}

90b7c468bbe235da45b3ccca578788c138b2633c
Alireza44431 <117797481+Alireza44431@users.noreply.github.com>
2023-04-18 22:14:41 +0330

    Add files via upload

---
+++ T_3/T_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("plese input your number : ");
+            int x=Convert.ToInt32(Console.ReadLine());
+            switch (x) 
+            {
+                case 1:
+                    Console.WriteLine("hello");
+                    break;
+                case 2:
+                    Console.WriteLine("goodbye");
+                    break;
+                 default: 
+                    Console.WriteLine("bye");
+                    break;
+            }
+        }
+    }
+}

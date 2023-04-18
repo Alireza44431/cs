e1f2b30309d581c1bf77dc74d198002dc58c737b
Alireza44431 <117797481+Alireza44431@users.noreply.github.com>
2023-04-18 22:13:38 +0330

    Add files via upload
    Taklif_2

---
+++ T_2/T_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("plese input your number : ");
+            int x=Convert.ToInt32(Console.ReadLine());
+            if (x == 1)
+            {
+                Console.WriteLine("hello");
+            }
+            else if (x == 2) 
+            {
+                Console.WriteLine("goodbye");
+            }
+            else 
+            {
+                Console.WriteLine("bye");
+            }
+        }
+    }
+}

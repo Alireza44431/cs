8fb59fb12cb19126d143fdb81ffd7df174e08b1e
Alireza44431 <117797481+Alireza44431@users.noreply.github.com>
2023-04-18 22:12:17 +0330

    T
    Taklif_1

---
+++ T_1/T_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("plese input your number : ");
+            int x=Convert.ToInt32(Console.ReadLine());
+            if(x>0) 
+            {
+                Console.WriteLine("this number is Positive");
+            }
+            else if(x==0) 
+            {
+                Console.WriteLine("this number is Zero");
+            }
+            else
+            {
+                Console.WriteLine("this number is Negative");
+            }
+            }
+    }
+}
